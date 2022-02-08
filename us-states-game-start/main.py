4b2e528d6218f500c53b39a1cda24bf381a6e692
joe rae <josephrae@gmail.com>
2022-02-07 22:32:35 -0500

    Initial commit

---
+++ us-states-game-start/main.py
@@ -0,0 +1,26 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+
+
+def get_mouse_click_coor(x, y):
+    print(x, y)
+
+
+turtle.onscreenclick(get_mouse_click_coor)
+
+turtle.mainloop()
+
+
+answer_state = screen.textinput(title="Guess The State", prompt="What's another state's name?")
+
+
+
+
+
